a84e12e883811b27714e4db5fc3f15c5bed61e2a
yosergiovc <104660842+yosergiovc@users.noreply.github.com>
2022-05-01 01:32:51 -0500

    Update Grafica.hs
    Le puse una función que regresa los vértices dada una gráfica.

--- src/Grafica.hs
+++ src/Grafica.hs
@@ -8,3 +8,7 @@ type Vertice = String
 type Adyacencias = [Vertice]
 
 type Grafica = [(Vertice, Adyacencias)]
+
+--Regresa una lista con los nombres de los vértices de la gráfica
+vertices :: Grafica -> [Vertice] 
+vertices grafica = [x | (x,_) <- grafica]
